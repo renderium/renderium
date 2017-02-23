207679b241cb594150f602638eb7d1ba4a0a6952
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-02-23 13:16:12 +0300

    Implemented base layer

---
+++ src/base-layer/index.js
@@ -0,0 +1,110 @@
+import leftPad from 'left-pad'
+
+class BaseLayer {
+  constructor ({ width, height }) {
+    this.canvas = document.createElement('canvas')
+    this.scale({ width, height })
+    this.components = []
+    this.stats = {}
+    this._shouldRedraw = false
+  }
+
+  scale ({ width, height }) {
+    this.width = width || BaseLayer.DEFAULT_WIDTH
+    this.height = height || BaseLayer.DEFAULT_HEIGHT
+
+    this.canvas.removeAttribute('width')
+    this.canvas.removeAttribute('height')
+    this.canvas.removeAttribute('style')
+
+    this.canvas.width = this.width
+    this.canvas.height = this.height
+
+    this.applyStyles()
+
+    this.forceRedraw()
+  }
+
+  applyStyles () {
+    this.canvas.style.width = `${this.width}px`
+    this.canvas.style.height = `${this.height}px`
+    this.canvas.style.position = 'absolute'
+    this.canvas.style.top = 0
+    this.canvas.style.left = 0
+    this.canvas.style.right = 0
+    this.canvas.style.bottom = 0
+  }
+
+  clear () {
+    this.clearStats()
+  }
+
+  redraw () {
+    for (var i = 0; i < this.components.length; i++) {
+      var component = this.components[i]
+      component.plot(this)
+      component.draw(this)
+    }
+    this._shouldRedraw = false
+  }
+
+  forceRedraw () {
+    this._shouldRedraw = true
+  }
+
+  shouldRedraw () {
+    for (var i = 0; i < this.components.length; i++) {
+      var component = this.components[i]
+      if (component.shouldRedraw()) {
+        return true
+      }
+    }
+    return this._shouldRedraw
+  }
+
+  addComponent (component) {
+    var idx = this.components.indexOf(component)
+    if (idx !== -1) {
+      throw new Error(`component ${component.constructor.name} has already been added to layer`)
+    }
+    if (typeof component.plot !== 'function' || typeof component.draw !== 'function' || typeof component.shouldRedraw !== 'function') {
+      throw new Error(`component ${component.constructor.name} has not implemented Component interface`)
+    }
+    this.components.push(component)
+    this.forceRedraw()
+  }
+
+  removeComponent (component) {
+    var idx = this.components.indexOf(component)
+    if (idx !== -1) {
+      this.components.splice(idx, 1)
+      this.forceRedraw()
+    }
+  }
+
+  clearStats () {
+    for (var methodName in this.stats) {
+      this.stats[methodName] = 0
+    }
+  }
+
+  collectStats (methodName) {
+    this.stats[methodName]++
+  }
+
+  formatStats () {
+    var result = []
+    var maxStringLength = 20
+
+    for (var methodName in this.stats) {
+      result.push(methodName + leftPad(this.stats[methodName], maxStringLength - methodName.length))
+    }
+
+    return result
+  }
+}
+
+BaseLayer.DEFAULT_WIDTH = 100
+BaseLayer.DEFAULT_HEIGHT = 100
+
+export default BaseLayer
