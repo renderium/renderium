b3719e671ef3108ebb9fb8ec6b50537e6bc66934
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-02-28 20:36:29 +0300

    Arc example

---
+++ examples/arc.js
@@ -0,0 +1,29 @@
+function Arc (options) {
+  this.position = options.position
+  this.color = options.color
+  this.radius = options.radius
+  this.startAngle = options.startAngle
+  this.endAngle = options.endAngle
+  this.width = options.width
+
+  this._shouldRedraw = true
+}
+
+Arc.prototype.shouldRedraw = function () {
+  return this._shouldRedraw
+}
+
+Arc.prototype.plot = function (layer) {}
+
+Arc.prototype.draw = function (layer) {
+  layer.drawArc({
+    position: this.position,
+    color: this.color,
+    radius: this.radius,
+    startAngle: this.startAngle,
+    endAngle: this.endAngle,
+    width: this.width
+  })
+
+  this._shouldRedraw = false
+}
