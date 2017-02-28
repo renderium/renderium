435296bce6439ca4404b33ea1f3a1682924e23ed
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-02-28 20:51:15 +0300

    Circle example

---
+++ examples/circle.js
@@ -0,0 +1,27 @@
+function Circle (options) {
+  this.position = options.position
+  this.color = options.color
+  this.fillColor = options.fillColor
+  this.radius = options.radius
+  this.width = options.width
+
+  this._shouldRedraw = true
+}
+
+Circle.prototype.shouldRedraw = function () {
+  return this._shouldRedraw
+}
+
+Circle.prototype.plot = function (layer) {}
+
+Circle.prototype.draw = function (layer) {
+  layer.drawCircle({
+    position: this.position,
+    color: this.color,
+    fillColor: this.fillColor,
+    radius: this.radius,
+    width: this.width
+  })
+
+  this._shouldRedraw = false
+}
