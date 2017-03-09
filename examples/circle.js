b77cf32f86e395728ad21a1ab49b0b47e546a5c6
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-03-09 20:52:37 +0300

    Animate circle example

--- examples/circle.js
+++ examples/circle.js
@@ -1,29 +1,65 @@
+/*
+global Animation
+*/
+
 function Circle (options) {
   this.position = options.position
   this.color = options.color
   this.fillColor = options.fillColor
-  this.radius = options.radius
+  this.minRadius = options.minRadius
+  this.maxRadius = options.maxRadius
   this.width = options.width
+  this.duration = options.duration
+  this.delay = options.delay
 
-  this._shouldRedraw = true
+  this._radius = this.minRadius
+
+  this.animationDelay = new Animation({
+    duration: this.delay
+  })
+  this.animationGrowUp = new Animation({
+    duration: this.duration,
+    handler: this._growUpHanlder.bind(this)
+  })
+  this.animationGrowDown = new Animation({
+    duration: this.duration,
+    handler: this._growDownHanlder.bind(this)
+  })
+  this.animationDelay.queue(this.animationGrowUp)
+  this.animationGrowUp.queue(this.animationGrowDown)
+  this.animationGrowDown.queue(this.animationDelay)
 }
 
 Circle.prototype.shouldRedraw = function () {
-  return this._shouldRedraw
+  return true
 }
 
-Circle.prototype.onadd = function (layer) {}
-Circle.prototype.onremove = function (layer) {}
-Circle.prototype.plot = function (layer) {}
+Circle.prototype.onadd = function (layer) {
+  this.animation.start()
+}
+
+Circle.prototype.onremove = function (layer) {
+  this.animation.stop()
+}
+
+Circle.prototype.plot = function (layer, time) {
+  this.animation.animate(time)
+}
 
 Circle.prototype.draw = function (layer) {
   layer.drawCircle({
     position: this.position,
     color: this.color,
     fillColor: this.fillColor,
-    radius: this.radius,
+    radius: this._radius,
     width: this.width
   })
+}
 
-  this._shouldRedraw = false
+Circle.prototype._growUpHanlder = function (t) {
+  this._radius = this.minRadius + t * (this.maxRadius - this.minRadius)
+}
+
+Circle.prototype._growDownHanlder = function (t) {
+  this._radius = this.maxRadius - t * (this.maxRadius - this.minRadius)
 }
